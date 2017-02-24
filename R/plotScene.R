1bc85f64b7f6687726d94fd80e38af530371260a
unknown <awaananen@CBG002822.chicagobotanic.org>
2017-02-24 11:55:54 -0600

    update plotScene and plot3Dscene parameter options (axis and label cex)

--- R/plotScene.R
+++ R/plotScene.R
@@ -3,8 +3,6 @@
 ##' @title graphical visualization of a mating scene object
 ##' @param scene a matingScene object
 ##' @param dimension what dimension(s) of the mating scene should be visualized. Possible dimensions are 't' for temporal, 's' for spatial, 'mt' for mating type, and 'auto' (the default). For dimension = 'auto', all dimensions represented in the mating scene object will be plotted.
-##' @param opening the number of days to adjust the start date displayed for the temporal dimension. Start date defaults to minimum day of year of start date in mating scene object.
-##' @param closing the number of days to adjust the end date displayed for the temporal dimension. End date defaults to maximum day of year end date in mating scene object.
 ##' @param dailyPoints logical indicating whether daily counts of individuals should be displayed for plots of the temporal dimension
 ##' @param drawQuartiles logical indicating whether vertical lines should be drawn at population peak (see details) or quartiles
 ##' @param sortBy character indicating which columns to sort segments of flowering schedule by. Defaults to 'start', then 'end'. Up to three variables may be specified.
@@ -16,8 +14,8 @@
 ##' @param ylab.spat character label for y-axis of spatial dimension plots. If NULL, defaults to 'northing'.
 ##' @param pch specify point type to be used in plots. Defaults to pch = 19 (filled-in circle). If NULL, points will be labeled with their id.
 ##' @param pt.cex specify point expansion factor (point size relative to device default)
-##' @param text.cex specify text expansion factor (text size relative to device default)
-##' @param plot.lim.spat if TRUE, spatial plot limits for lists of scenes are set by the maximum from all scenes
+##' @param label.cex specify text expansion factor (text size relative to device default)
+##' @param plot.lim.zoom if TRUE, spatial plot limits for lists of scenes are set by the maximum from all scenes
 ##' @param quartile.lwd if \code{drawQuartiles} = TRUE, specifies weight of quartile and peak lines relative to device default.
 ##' @param quartile.col if \code{drawQuartiles} = TRUE, specifies color of quartile lines, defaults to 'gray81'.
 ##' @param peak.col if \code{drawQuartiles} = TRUE, specify color of peak lines, defaults to 'gray27'.
@@ -39,19 +37,18 @@
 ##'
 ##'
 plotScene <- function(scene, dimension = "auto",
-                      opening = NULL, closing = NULL,
                       dailyPoints = TRUE, drawQuartiles = TRUE,
                       sortBy = c('start','end'), colorBy = NULL,
-                      sub= NULL, N = 3, label.sub = TRUE,
+                      sub = NULL, N = 3, label.sub = TRUE,
                       xlab.spat = NULL, ylab.spat = NULL,
-                      pch = 19, pt.cex = 0.75, text.cex = 0.8,
-                      plot.lim.spat  = TRUE,
+                      pch = 19, pt.cex = 0.75, label.cex = 0.8,
+                      plot.lim.zoom  = FALSE, # this could use a better name, but I don't know what
                       quartile.lwd = 1, quartile.col = 'gray55', peak.col = 'gray27',
                       labelID = FALSE, mt1 = 'F', mt2 = 'M', leg.ncol = 1,
                       ...){
 
   dimension <- match.arg(dimension, c("auto", "t", "s", "mt"),several.ok = TRUE)
-  par.orig <- par("mar", "oma", "mfrow", "xpd")
+  par.orig <- par("mfrow", "xpd")
   on.exit(par(par.orig))
 
   if (!is.list(scene[[1]])){
@@ -73,14 +70,6 @@ plotScene <- function(scene, dimension = "auto",
   nr <- length(scene)
   nc <- sum(temp,spat,comp)
   par(mfrow = c(nr,nc), xpd = F)
-  # if(is.null(colorBy)){
-  #   par(oma = c(5,3,4,1))
-  # } else if (!is.null(colorBy) | labelID) {
-  #   par(oma = c(8,3,4,1))
-  # }
-  # if(!is.null(oma)){
-  #   par(oma = oma)
-  # }
 
   if(spat){
     emin <- min(unlist(lapply(scene, function(x) x['x'])))
@@ -131,11 +120,7 @@ plotScene <- function(scene, dimension = "auto",
       scene.i <- scene.i[order(orderVars[,1],orderVars[,2], orderVars[,3]),]
     }
     scene.i$index <- seq_along(scene.i[, 1])
-    # if (is.null(mar)){
-    #   par(mar = c(0.25,3.25,1,1))
-    # } else{
-    #   par(mar = mar)
-    # }
+
     if (!is.null(colorBy)){
       if (is.numeric(scene.i[,colorBy])){
         palette(colorRampPalette(c('blue','red'))(9))
@@ -161,32 +146,30 @@ plotScene <- function(scene, dimension = "auto",
     if (temp){
       if (labelID){
         plot.default(scene.i[, 'start'] + attr(scene.i, "origin"), scene.i$index, ylim = c(1,count), xlim = c(minstart, maxend), type = "n", xlab = '', ylab = "",xaxt = 'n',yaxt = 'n', ...)
-
         segments(scene.i[, 'start'] + attr(scene.i, "origin"), scene.i$index, scene.i[, 'end'] + attr(scene.i, "origin"),scene.i$index, col = cols.seg, cex = 3, ...)
-        axis(2, labels = scene.i$id, at = scene.i$index, las = 1, cex.axis = 0.75)
+        axis(2, labels = scene.i$id, at = scene.i$index, las = 1, ...)
         mtext(attr(scene.i,'originalNames')[1],side = 2,adj = 0.5, cex = 0.75, line = 7.5)
       } else {
         plot.default(scene.i[, 'start'] + attr(scene.i, "origin"), scene.i$index, ylim = c(1,count), xlim = c(minstart, maxend), type = "n", xlab = '', ylab = "",xaxt = 'n',yaxt = 'n', ...)
         segments(scene.i[, 'start'] + attr(scene.i, "origin"), scene.i$index, scene.i[, 'end'] + attr(scene.i, "origin"),scene.i$index, col = cols.seg, cex = 3, ...)
         mtext('count',side = 2,adj = 0.5, cex = 0.75, line = 2.5)
-        axis(2)
+        axis(2,...)
       }
       mtext(names(scene)[i],side = 2,adj = 0.5, cex = 0.75, line = 5, font = 2, las = 3)
 
       if (i == nr){
         datLabs <- seq(minstart,maxend, by = 7)
-        axis(1, at = datLabs, labels = format(datLabs,format = "%b %d"), tick=0.25, cex.axis = 0.9)
+        axis(1, at = datLabs, labels = format(datLabs,format = "%b %d"), tick=0.25, ...)
         mtext('date',side = 1,adj = 0.5, cex = 0.75, line = 3)
       }
-      # if (i == 1 & nc > 1){
-      #   mtext('temporal',side = 3, adj = 0.5, line = 1.5)
-      # }
+
       if (!is.null(sub)){
         segments(scene.i[scene.i$id %in% sub, 'start'] + attr(scene.i, "origin"), scene.i[scene.i$id %in% sub, 'index'], scene.i[scene.i$id %in% sub, 'end'] + attr(scene.i, "origin"),scene.i[scene.i$id %in% sub, 'index'], col = cols.sub, ...)
         if(label.sub){
-          text(scene.i[scene.i$id %in% sub, 'start'] + attr(scene.i, "origin")-0.02*closing, scene.i[scene.i$id %in% sub, 'index'], scene.i[scene.i$id %in% sub, 'id'], cex = text.cex)
+          text(scene.i[scene.i$id %in% sub, 'start'] + attr(scene.i, "origin") - 2, scene.i[scene.i$id %in% sub, 'index'], scene.i[scene.i$id %in% sub, 'id'], cex = label.cex)
         }
       }
+
       if (dailyPoints == TRUE){
         rbd <- receptivityByDay(scene.i)
         fl.density <- colSums(rbd)
@@ -208,36 +191,29 @@ plotScene <- function(scene, dimension = "auto",
     if (spat){
       if (is.null(xlab.spat)) xlab.spat <- 'easting'
       if (is.null(ylab.spat)) ylab.spat <- 'northing'
-      if(!plot.lim.spat){
-        # if (is.null(mar)){
-        #   par(mar = c(1.25,3.25,1,1))
-        # } else{
-        #   par(mar = mar)
-        # }
+      if(plot.lim.zoom){
         plot.default(scene.i[, 'x'], scene.i[, 'y'], type = "n", xlab = '', ylab = "",xaxt = 'n', asp = 1, cex = pt.cex, col = cols.pt, ...)
         if(i != nr){
-          axis(1, cex.axis = 0.75, padj = -1.5)
+          axis(1,...)
         }
       } else {
         plot.default(scene.i[, 'x'], scene.i[, 'y'], type = "n",xlim = c(emin,emax), ylim = c(nmin,nmax), ylab = "", xlab = '', xaxt = 'n', asp = 1, cex = pt.cex, col = cols.pt, ...)
       }
       mtext(ylab.spat,side = 2,adj = 0.5, cex = 0.75, line = 2.5)
       if (i == nr){
-        axis(1)
+        axis(1,...)
         mtext(xlab.spat,side = 1,adj = 0.5, cex = 0.75, line = 3)
       }
-      # if(i == 1 & nc > 1){
-      #   mtext('spatial',side = 3, adj = 0.5, line = 1.5)
-      # }
+
       if (is.null(pch)) {
-        text(scene.i[, 'x'], scene.i[, 'y'], scene.i[, 'id'],cex = text.cex, col = cols.pt, ...)
+        text(scene.i[, 'x'], scene.i[, 'y'], scene.i[, 'id'],cex = label.cex, col = cols.pt, ...)
       } else {
         points(scene.i[, 'x'], scene.i[, 'y'], pch = pch, cex = pt.cex, col = cols.pt, ...)
       }
       if (!is.null(sub)){
         scene.i.sub <- scene.i[scene.i[, 'id'] %in% sub, ]
         if(label.sub){
-          text(scene.i.sub[, 'x'], scene.i.sub[, 'y'], scene.i.sub[, 'id'], pos = 3,xpd = T,cex = text.cex, ...)
+          text(scene.i.sub[, 'x'], scene.i.sub[, 'y'], scene.i.sub[, 'id'], pos = 3,xpd = T,cex = label.cex, ...)
         }
         points(scene.i.sub[, 'x'], scene.i.sub[, 'y'], pch = 19,col = cols.sub, cex = pt.cex, ...)
       }
@@ -273,17 +249,15 @@ plotScene <- function(scene, dimension = "auto",
         ptWt$scale <- (ptWt$id - min(ptWt$id)) / diff(range(ptWt$id)) + 1
         plot(ptWt$s1, ptWt$s2, cex = ptWt$scale, pch = pch, xlim = c(smin, smax), ylim = c(smin,smax), ylab = "",xlab = '', xaxt = 'n', yaxt = 'n')
         mtext('s2',side = 2,adj = 0.5, cex = 0.75, line = 2.5)
-        axis(2, at = smin:smax, labels = smin:smax, tick = 0.25)
+        axis(2, at = smin:smax, labels = smin:smax, tick = 0.25,...)
         leg.text <- levels(as.factor(ptWt$id))
         legend('topleft',legend = leg.text, pt.cex = 1+(as.numeric(leg.text) - min(as.numeric(leg.text)))/diff(range(as.numeric(leg.text))), pch = pch, title = 'count')
         if (i == nr){
           mtext('s1',side = 1,adj = 0.5, cex = 0.75, line = 3)
-          axis(1, at = smin:smax, labels = smin:smax)
+          axis(1, at = smin:smax, labels = smin:smax,...)
         }
       }
-      # if(i == 1 & nc > 1){
-      #   mtext('mating type',side = 3, adj = 0.5, line = 1.5)
-      # }
+
       if (temp == F & spat == F){
         mtext(names(scene)[i],side = 2,adj = 0.5, cex = 0.75, line = 5, font = 2, las = 3)
       }
