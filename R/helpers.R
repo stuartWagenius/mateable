f4fdc1e997bfc2e5217a8ff68fe8792bbeed3982
amywaananen <awaananen@chicagobotanic.org>
2017-04-05 09:17:58 -0500

    update matingSummary.df helper function

--- R/helpers.R
+++ R/helpers.R
@@ -6,11 +6,13 @@ matingSummary.df <- function(matSum){
   if(is.list(matSum[[1]][1])){
     l <- lapply(matSum, unlist)
     df <- as.data.frame(do.call(rbind, l))
-    df$popSt <- as.Date(df$popSt, origin = '1970-01-01')
-    df$peak <- as.Date(df$peak, origin = '1970-01-01')
-    df$meanSD <- as.Date(df$meanSD, origin = '1970-01-01')
-    df$meanED <- as.Date(df$meanED, origin = '1970-01-01')
-    df$popEnd <- as.Date(df$popEnd, origin = '1970-01-01')
+    if('popSt' %in% colnames(df)){
+      df$popSt <- as.Date(df$popSt, origin = '1970-01-01')
+      df$peak <- as.Date(df$peak, origin = '1970-01-01')
+      df$meanSD <- as.Date(df$meanSD, origin = '1970-01-01')
+      df$meanED <- as.Date(df$meanED, origin = '1970-01-01')
+      df$popEnd <- as.Date(df$popEnd, origin = '1970-01-01')
+    }
     df$id <- rownames(df)
     if (!all(df$id == as.character(df$year))){
       df <- df[,c(ncol(df),1:(ncol(df)-1))]
