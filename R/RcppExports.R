cbb4ac7dcdbaf16a17a23e8d853b38fff7728369
Amy W <amy.waananen@gmail.com>
2020-12-11 15:00:34 -0600

    Update for Rcpp routine registration

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,38 +2,38 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 pair_si_ech <- function(s1, s2) {
-    .Call('_mateable_pair_si_ech', PACKAGE = 'mateable', s1, s2)
+    .Call(`_mateable_pair_si_ech`, s1, s2)
 }
 
 pair_dioecious <- function(s1) {
-    .Call('_mateable_pair_dioecious', PACKAGE = 'mateable', s1)
+    .Call(`_mateable_pair_dioecious`, s1)
 }
 
 row_medians <- function(toSort) {
-    .Call('_mateable_row_medians', PACKAGE = 'mateable', toSort)
+    .Call(`_mateable_row_medians`, toSort)
 }
 
 row_kth <- function(toSort, k) {
-    .Call('_mateable_row_kth', PACKAGE = 'mateable', toSort, k)
+    .Call(`_mateable_row_kth`, toSort, k)
 }
 
 kemp_ind <- function(byDay, starts, ends, durs, compSelf = FALSE) {
-    .Call('_mateable_kemp_ind', PACKAGE = 'mateable', byDay, starts, ends, durs, compSelf)
+    .Call(`_mateable_kemp_ind`, byDay, starts, ends, durs, compSelf)
 }
 
 daysSync_noself <- function(starts, ends, n) {
-    .Call('_mateable_daysSync_noself', PACKAGE = 'mateable', starts, ends, n)
+    .Call(`_mateable_daysSync_noself`, starts, ends, n)
 }
 
 daysSync_self <- function(starts, ends, n) {
-    .Call('_mateable_daysSync_self', PACKAGE = 'mateable', starts, ends, n)
+    .Call(`_mateable_daysSync_self`, starts, ends, n)
 }
 
 daysEither_noself <- function(starts, ends, n) {
-    .Call('_mateable_daysEither_noself', PACKAGE = 'mateable', starts, ends, n)
+    .Call(`_mateable_daysEither_noself`, starts, ends, n)
 }
 
 daysEither_self <- function(starts, ends, n) {
-    .Call('_mateable_daysEither_self', PACKAGE = 'mateable', starts, ends, n)
+    .Call(`_mateable_daysEither_self`, starts, ends, n)
 }
 
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -118,21 +118,3 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-
-static const R_CallMethodDef CallEntries[] = {
-    {"_mateable_pair_si_ech", (DL_FUNC) &_mateable_pair_si_ech, 2},
-    {"_mateable_pair_dioecious", (DL_FUNC) &_mateable_pair_dioecious, 1},
-    {"_mateable_row_medians", (DL_FUNC) &_mateable_row_medians, 1},
-    {"_mateable_row_kth", (DL_FUNC) &_mateable_row_kth, 2},
-    {"_mateable_kemp_ind", (DL_FUNC) &_mateable_kemp_ind, 5},
-    {"_mateable_daysSync_noself", (DL_FUNC) &_mateable_daysSync_noself, 3},
-    {"_mateable_daysSync_self", (DL_FUNC) &_mateable_daysSync_self, 3},
-    {"_mateable_daysEither_noself", (DL_FUNC) &_mateable_daysEither_noself, 3},
-    {"_mateable_daysEither_self", (DL_FUNC) &_mateable_daysEither_self, 3},
-    {NULL, NULL, 0}
-};
-
-RcppExport void R_init_mateable(DllInfo *dll) {
-    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
-}
